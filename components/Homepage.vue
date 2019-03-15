f88441d63b03be57e264509268c26b91a8c57b7e
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-03-15 16:31:59 +1100

    add images to click on in project section

--- components/Homepage.vue
+++ components/Homepage.vue
@@ -128,49 +128,49 @@
         <!-- thumb-carousel : starts -->
         <div id="thumb-carousel" class="owl-carousel">
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="http://placehold.it/700x700">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-Ecommerce.jpg">
               <img data-no-retina alt="Ecommerce" title="Ecommerce" class="img-responsive" src="~/assets/images/Project-Ecommerce.jpg">
               <div class="thumb-title"><h5><span>Ecommerce</span></h5></div>
             </a>
           </div>
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="http://placehold.it/700x700">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-Logo.jpg">
               <img data-no-retina alt="Logo" title="Logo" class="img-responsive" src="~/assets/images/Project-Logo.jpg">
               <div class="thumb-title"><h5><span>Logo</span></h5></div>
             </a>
           </div>
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="http://placehold.it/700x700">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-Responsive.jpg">
               <img data-no-retina alt="Responsive" title="Responsive" class="img-responsive" src="~/assets/images/Project-Responsive.jpg">
               <div class="thumb-title"><h5><span>Responsive</span></h5></div>
             </a>
           </div>
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="http://placehold.it/700x700">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-Branding.jpg">
               <img data-no-retina alt="Branding" title="Branding" class="img-responsive" src="~/assets/images/Project-Branding.jpg">
               <div class="thumb-title"><h5><span>Branding</span></h5></div>
             </a>
           </div>
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="http://placehold.it/700x700">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-Management.jpg">
               <img data-no-retina alt="Management" title="Management" class="img-responsive" src="~/assets/images/Project-Management.jpg">
               <div class="thumb-title"><h5><span>Management</span></h5></div>
             </a>
           </div>
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="http://placehold.it/700x700">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-Brochure.jpg">
               <img data-no-retina alt="Brochure" title="Brochure" class="img-responsive" src="~/assets/images/Project-Brochure.jpg">
               <div class="thumb-title"><h5><span>Brochure</span></h5></div>
             </a>
           </div>
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="~/assets/images/Project-Wireframe.jpg">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-Wireframe.jpg">
               <img data-no-retina alt="Wireframe" title="Wireframe" class="img-responsive" src="~/assets/images/Project-Wireframe.jpg">
               <div class="thumb-title"><h5><span>Wireframe</span></h5></div>
             </a>
           </div>
           <div class="item">
-            <a class="venobox" data-gall="thumb-gallery" href="http://placehold.it/700x700">
+            <a class="venobox" data-gall="thumb-gallery" href="/images/Project-BusinessCard.jpg">
               <img data-no-retina alt="Business Card" title="Business Card" class="img-responsive" src="~/assets/images/Project-BusinessCard.jpg">
               <div class="thumb-title"><h5><span>Business Card</span></h5></div>
             </a>
