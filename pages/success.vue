566643aeb770d097c5c3d33a82ec7d0d083a65ba
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-03-20 17:31:34 +1100

    update success page

--- pages/success.vue
+++ pages/success.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        <h1>Thank you for submitting this form.</h1>
+    <div class="container text-center">
+        <h1>Thank you for submitting this form. We will be in touch shortly.</h1>
     </div>
 </template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
