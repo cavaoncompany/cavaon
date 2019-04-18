d7b83c3e0f77011d764c4a328c04f4403c2eea40
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-04-18 17:59:54 +1000

    setting up hubspot

--- store/index.js
+++ store/index.js
@@ -1,4 +1,8 @@
 import axios from '~/plugins/axios'
+import request from 'request-promise'
+import { eventNames } from 'cluster';
+require('dotenv').config()
+
 export const state = () => ({
   emailProvider: {
     service: 'smtp',
@@ -49,9 +53,31 @@ export const actions = {
   async contactUs({ state, commit }, payload) {
     const path = '/.netlify/functions/contact'
     await sendEmail({ state, commit }, payload, path)
+    await tryHubspot()
   },
   async subsribeTo({ state, commit }, payload) {
     const path = '/.netlify/functions/blog'
     await sendEmail({ state, commit }, payload, path)
+  }
+}
+
+async function tryHubspot() {
+  try {
+    const endpoint = '/deals/v1/deal/recent/modified'
+
+    const deals = await request({
+      method: 'GET',
+      url: `https://api.hubapi.com${endpoint}`,
+      qs: {
+        hapikey: process.env.hubspotapikey,
+        count: 100,
+        since: '1463680280365'
+      },
+      json: true
+    })
+
+    console.log(deals)
+  } catch (e) {
+    alert(e)
   }
 }
