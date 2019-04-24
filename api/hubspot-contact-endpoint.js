7de23a4b4718e9dcca398808e9f696596be5f020
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-04-24 17:59:42 +1000

    update endpoint

---
+++ api/hubspot-contact-endpoint.js
@@ -0,0 +1,68 @@
+import errorHandler from '../helpers/errorHandler'
+import responseHandler from '../helpers/responseHandler'
+
+const Contacts = (api = null) => {
+
+  if (api === null) throw new Error('Request instance must be provided on constructor.')
+
+  return {
+    mapProperties(properties) {
+
+      return Object.keys(properties).map(property => ({
+        property: property,
+        value: properties[property]
+      }))
+    },
+    getAll(params) {
+      const defaultParams = {
+        count: null,
+        vidOffset: null,
+        ...params
+      }
+
+      return api.get('contacts/v1/lists/all/contacts/all', defaultParams)
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    },
+    getContactById(id, parameters = {}) {
+      return api.get(`contacts/v1/contact/vid/${id}/profile`, parameters)
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    },
+    getContactByEmail(email, parameters = {}) {
+      return api.get(`contacts/v1/contact/email/${email}/profile`, parameters)
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    },
+    createContact(properties) {
+      const mappedProperties = this.mapProperties(properties)
+
+      return api.post('contacts/v1/contact', { properties: [ ...mappedProperties ] })
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    },
+    updateContactById(properties, id) {
+      const mappedProperties = this.mapProperties(properties)
+
+      return api.post(`contacts/v1/contact/vid/${id}/profile`, { properties: [ ...mappedProperties ] })
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    },
+    updateContactByEmail(properties, email) {
+      const mappedProperties = this.mapProperties(properties)
+
+      return api.post(`contacts/v1/contact/email/${email}/profile`, { properties: [ ...mappedProperties ] })
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    },
+    createOrUpdateContact(properties, email) {
+      const mappedProperties = this.mapProperties(properties)
+
+      return api.post(`contacts/v1/contact/createOrUpdate/email/${email}`, { properties: [ ...mappedProperties ] })
+        .then(response => responseHandler(response))
+        .catch(error => errorHandler(error))
+    }
+  }
+}
+
+export default Contacts
