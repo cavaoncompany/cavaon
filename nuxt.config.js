50675a6355bc6788e26a0bde9e7834be89dc20e1
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-06-24 11:50:24 +1000

    update local server port

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,6 +97,9 @@ module.exports = {
   plugins: [
     { src: '~/plugins/tawkto.js', ssr: false }
   ],
+  server: {
+    port: 3002 // default: 3000
+  },
   serverMiddleware: [
     '~/api/nodemailer'
   ],
