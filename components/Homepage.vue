0b87063bb8d05c11eecb607724a59f41223d892a
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-07-02 14:42:59 +1000

    mailchimp

--- components/Homepage.vue
+++ components/Homepage.vue
@@ -352,7 +352,6 @@ import Services from './Services'
 import ProjectWithSlider from './ProjectWithSlider'
 import ArticleList from './blog/Article-list'
 import Footer from './Footer'
-import { setupMailchimpSignup } from '../api/mailchimpSignup.js'
 
 export default {
   components: {
@@ -434,7 +433,7 @@ export default {
     await this.$recaptcha.init()
   },
   mounted() {
-    setupMailchimpSignup()
+    this.setupMailchimpPopup()
   },
   methods: {
     openModal() {
@@ -487,6 +486,24 @@ export default {
         lastname: this.subscriberLastname
       }
       this.$store.dispatch('createSubscriber', subscriberInfo)
+    },
+    setupMailchimpPopup() {
+      const mailchimpConfig = {
+        baseUrl: 'mc.us18.list-manage.com',
+        uuid: '35f22d84b4fdb209da5977e3b',
+        lid: 'd5263ee7ce'
+      }
+
+      const chimpPopupLoader = document.createElement('script')
+      chimpPopupLoader.src = '//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js'
+      chimpPopupLoader.setAttribute('data-dojo-config', 'usePlainJson: true, isDebug: false')
+      const chimpPopup = document.createElement('script')
+      chimpPopup.appendChild(document.createTextNode('require(["mojo/signup-forms/Loader"], function (L) { L.start({"baseUrl": "' + mailchimpConfig.baseUrl + '", "uuid": "' + mailchimpConfig.uuid + '", "lid": "' + mailchimpConfig.lid + '"})});'))
+
+      chimpPopupLoader.onload = function () {
+        document.body.appendChild(chimpPopup)
+      }
+      document.body.appendChild(chimpPopupLoader)
     },
     async onSubmit() {
       try {
