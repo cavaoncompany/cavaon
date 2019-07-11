7167a307e82503548660f10e7f41150e8cdb353a
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-07-11 11:19:05 +1000

    update flowchart section

--- components/Flowchart.vue
+++ components/Flowchart.vue
@@ -1,29 +1,22 @@
 <template>
   <section id="flowchart" class="page page-section" @mouseover="changeUrl('services')">
     <div class="container">
-      <div class="row">
-        <div class="col-12 col-lg-4 flowchart-left">
-          <article class="title">
-            <h2>{{ flowchart.title }}</h2>
-          </article>
-          <img :src="flowchart.leftImage" alt="timeline">
-        </div>
-        <div class="col-12 col-lg-8">
-          <div class="row flowchart-items">
-            <div v-for="(item, index) in flowchart.steps" :key="index">
-              <div v-if="item.id % 2 === 0" class="col-12 col-sm-6">
-                <img :src="item.image" :alt="item.title">
-              </div>
-              <div class="col-12 col-sm-6">
-                <h3>{{ item.id }}</h3>
-                <h3>{{ item.title }}</h3>
-                <p>{{ item.description }}</p>
-                <div v-if="index < (flowchart.steps.length - 1)" class="timeline" />
-              </div>
-              <div v-if="item.id % 2 !== 0" class="col-12 col-sm-6">
-                <img :src="item.image" :alt="item.title">
-              </div>
+      <h2>{{ flowchart.title }}</h2>
+      <div class="liner color-bg" />
+      <div class="row flowchart-content">
+        <div v-for="(item, i) in flowchart.steps" :key="i" class="card-container col-md-6 col-lg-3">
+          <div class="card">
+            <img class="card-img-top" :src="item.image" :alt="item.title">
+            <div class="card-body">
+              <h5 class="card-title">{{ item.title }}</h5>
+              <div class="liner color-bg" />
+              <p class="card-text">
+                {{ item.description }}
+              </p>
             </div>
+          </div>
+          <div v-if="i < flowchart.steps.length - 1" class="next-item">
+            <i class="fa fa-chevron-right"></i>
           </div>
         </div>
       </div>
@@ -35,15 +28,10 @@
 import flowchart from '../static/content/flowchart.json'
 
 export default {
-  name: 'Flowchart',
   data() {
     return {
-      flowchart: flowchart
-    }
-  },
-  methods: {
-    changeUrl: function (link) {
-      history.pushState({}, null, '/' + link)
+      flowchart: flowchart,
+      lastcard: false
     }
   }
 }
@@ -52,37 +40,56 @@ export default {
 <style>
   #flowchart {
     margin-top: 40px;
+    font-size: 14px;
+    color: #222222;
+    text-align: center;
   }
-  #flowchart article {
-    border-left: 1px solid #FFC716;
-    margin-top: -120px;
-    padding-top: 140px;
-    padding-left: 15px;
+  #flowchart h2 {
+    font-size: 24px;
   }
-  #flowchart article h2 {
-    font-size: 30px;
-    font-weight: 600;
-    margin-bottom: 0;
+  #flowchart .flowchart-content {
+    margin-top: 40px;
   }
-  #flowchart .flowchart-left {
+  #flowchart .flowchart-content .liner {
+    width: 39px;
+  }
+  #flowchart h5 {
+    height: 1rem;
+    font-size: 16px;
+  }
+  #flowchart .card {
+    box-shadow: 2px 2px 10px rgba(228, 214, 233, 0.8);
+    border: none;
+    padding: 10px;
+    height: 100%;
+  }
+  #flowchart .card-body {
+    color: #494949;
+  }
+  #flowchart .card-container {
+    position: relative;
+  }
+  #flowchart .card-text {
+    padding-top: 10px;
+  }
+  #flowchart .fa {
+    color: #FFC716;
+    font-size: 20px;
+    font-weight: 300;
+  }
+  #flowchart .next-item {
+    height: 50px;
+    border-radius: 50%;
+    width: 50px;
+    box-shadow: 7px 2px 7px 0px rgba(228, 214, 233, 0.8);
+    position: absolute;
+    text-align: center;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
-  }
-  #flowchart .flowchart-left img {
-    width: 60%;
-  }
-  .flowchart-items img {
-    width: 100%;
-    height: auto;
-  }
-  .flowchart-items h3 {
-      font-size: 16px;
-      font-weight: 500;
-  }
-  .flowchart-items .timeline {
-    border-left: 1px solid #FFC716;
-    height: 100px;
-    margin-left: 25px;
+    justify-content: center;
+    top: 130px;
+    right: -5px;
+    background-color: #fff;
+    padding-left: 10px
   }
 </style>
