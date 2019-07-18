b09e5215e82d86542e05531be39f89d87e0a9225
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-07-18 14:58:08 +1000

    update web dev formatting

--- pages/services/web-development-and-technical-support.vue
+++ pages/services/web-development-and-technical-support.vue
@@ -8,12 +8,13 @@
       <p>{{ services.category }}</p>
       <h1>{{ services.title }}</h1>
     </div>
-    <main class="container services-web-development-main-content">
-      <h2>
+    <main class="services-web-development-main-content">
+      <div class="container">
+        <h2>
         {{ services.subtitle }}
       </h2>
       <div class="liner color-bg" />
-      <p>
+      <p class="services-web-intro">
         {{ services.intro }}
       </p>
       <img :src="services.image1" :alt="services.title">
@@ -21,7 +22,7 @@
         <div class="col-md-6">
           <img :src="services.image2" :alt="services.header1">
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 service-web-details-text">
           <h3>
             {{ services.header1 }}
           </h3>
@@ -29,7 +30,7 @@
             {{ services.box1 }}
           </p>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 service-web-details-text">
           <h3>
             {{ services.header2 }}
           </h3>
@@ -41,16 +42,21 @@
           <img :src="services.image3" :alt="services.header1">
         </div>
       </div>
+      </div>
       <div class="purple-bg row">
-        <div v-for="(box, i) in services.services" :key="i" class="service-web-box col-md-6">
-          <img :src="box.image" :alt="box.header">
-          <h3>
-            {{ box.header }}
-          </h3>
-          <div class="liner color-bg" />
-          <p>
-            {{ box.description }}
-          </p>
+        <div class="container">
+          <div v-for="(box, i) in services.services" :key="i" class="service-web-box col-md-6">
+            <div class="service-web-border-box">
+              <img :src="box.image" :alt="box.header">
+              <h3>
+                {{ box.header }}
+              </h3>
+              <div class="liner color-bg" />
+              <p>
+                {{ box.description }}
+              </p>
+            </div>
+        </div>
         </div>
       </div>
       <!-- <BookAMeeting /> -->
@@ -114,6 +120,9 @@ export default {
     margin: 0 auto;
     width: 350px;
 }
+.services-web-intro {
+  margin-bottom: 70px;
+}
 .services-web-development main {
   margin-top: 70px;
   font-size: 16px;
@@ -123,6 +132,11 @@ export default {
   font-weight: 500;
   width: 60%;
 }
+.services-web-development main h3,
+.service-web-box h3 {
+  font-size: 16px;
+  font-weight: 500;
+}
 .services-web-development main .liner {
   width: 36px;
   margin: 25px 0;
@@ -130,5 +144,28 @@ export default {
 .services-web-development main img {
   width: 100%;
   margin-bottom: 70px;
+}
+.service-web-details-text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 70px;
+}
+.service-web-box {
+  margin: 70px 0;
+  height: 80%;
+}
+.service-web-box .service-web-border-box {
+  background-color: #fff;
+  width: 90%;
+  padding: 30px;
+  height: 100%;
+}
+.services-web-development main .service-web-box img {
+  margin-bottom: 40px;
+}
+.services-web-development main .service-web-box .liner {
+  width: 36px;
+  margin: 10px 0 25px;
 }
 </style>
