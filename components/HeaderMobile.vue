c995a2c3dc31d10d106ec8d299c709573d8dbab5
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-07-18 14:17:13 +1000

    update homepage scroll

--- components/HeaderMobile.vue
+++ components/HeaderMobile.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- mobile only navigation : starts -->
   <nav class="mobile-nav hidden-md hidden-lg">
-    <ul v-if="page !== 'homepage'" class="slimmenu">
+    <ul v-if="page !== 'home'" class="slimmenu">
       <li>
         <a href="/" aria-label="Scroll to Services">SERVICES</a>
       </li>
@@ -19,21 +19,21 @@
       </li>
     </ul>
 
-    <ul v-if="page === 'homepage'" class="slimmenu">
+    <ul v-if="page === 'home'" class="slimmenu">
       <li>
-        <a  href="#services" aria-label="Scroll to Services">SERVICES</a>
+        <a class="scroll" href="#services" aria-label="Scroll to Services">SERVICES</a>
       </li>
       <li>
-        <a  href="#caseStudies" aria-label="Scroll to Case Studies">CASE STUDIES</a>
+        <a class="scroll" href="#caseStudies" aria-label="Scroll to Case Studies">CASE STUDIES</a>
       </li>
       <li>
         <a href="/blog" aria-label="Go to blog">BLOG</a>
       </li>
       <li>
-        <a  href="#about-mobile" aria-label="Scroll to About">ABOUT</a>
+        <a class="scroll" href="#about-mobile" aria-label="Scroll to About">ABOUT</a>
       </li>
       <li>
-        <a  href="#contact" aria-label="Scroll to Contact Form">CONTACT</a>
+        <a class="scroll" href="#contact" aria-label="Scroll to Contact Form">CONTACT</a>
       </li>
     </ul>
   </nav>
