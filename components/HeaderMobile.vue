d63341aedf51dd4504a094104075203a7d2cb910
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-07-18 14:11:43 +1000

    scroll

--- components/HeaderMobile.vue
+++ components/HeaderMobile.vue
@@ -21,19 +21,19 @@
 
     <ul v-if="page === 'homepage'" class="slimmenu">
       <li>
-        <a class="scroll" href="#services" aria-label="Scroll to Services">SERVICES</a>
+        <a  href="#services" aria-label="Scroll to Services">SERVICES</a>
       </li>
       <li>
-        <a class="scroll" href="#caseStudies" aria-label="Scroll to Case Studies">CASE STUDIES</a>
+        <a  href="#caseStudies" aria-label="Scroll to Case Studies">CASE STUDIES</a>
       </li>
       <li>
         <a href="/blog" aria-label="Go to blog">BLOG</a>
       </li>
       <li>
-        <a class="scroll" href="#about-mobile" aria-label="Scroll to About">ABOUT</a>
+        <a  href="#about-mobile" aria-label="Scroll to About">ABOUT</a>
       </li>
       <li>
-        <a class="scroll" href="#contact" aria-label="Scroll to Contact Form">CONTACT</a>
+        <a  href="#contact" aria-label="Scroll to Contact Form">CONTACT</a>
       </li>
     </ul>
   </nav>
